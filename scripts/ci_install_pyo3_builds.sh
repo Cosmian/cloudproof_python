949eca815bba7203a63f06948cff6d5535037ec9
Hugo Rosenkranz-Costa <hugo.rosenkranz@cosmian.com>
2023-02-02 16:50:15 +0100

    ci: fix dependencies logic in CI

--- scripts/ci_install_pyo3_builds.sh
+++ scripts/ci_install_pyo3_builds.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-set -eux
+set -ux
 
 install_lib() {
-    wget "https://package.cosmian.com/$1/$2/linux.zip"
-    unzip -o linux.zip
-    pip install --force-reinstall x86_64-unknown-linux-gnu/python-x86_64-unknown-linux-gnu/$1*.whl
+    wget "https://package.cosmian.com/$1/$2/linux.zip" && \
+    unzip -o linux.zip && \
+    pip install --force-reinstall x86_64-unknown-linux-gnu/python-x86_64-unknown-linux-gnu/$1*.whl && \
     rm linux.zip && rm -rf x86_64-unknown-linux-gnu
 }
 
-install_lib "cover_crypt" ${COVER_CRYPT_TAG:-last_build}
-install_lib "findex" ${FINDEX_TAG:-last_build}
+install_lib "cover_crypt" "v10.0.0"
+if [ $? -ne 0 ]; then
+    install_lib "cover_crypt" "last_build"
+fi
+
+install_lib "findex" "v2.0.1"
+if [ $? -ne 0 ]; then
+    install_lib "findex" "last_build"
+fi
