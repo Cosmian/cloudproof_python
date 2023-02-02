b119bf610eeccf54381b0c4a63901151f12a3e35
Hugo Rosenkranz-Costa <hugo.rosenkranz@cosmian.com>
2023-02-02 11:55:31 +0100

    ci: fix unzip overwrite

--- scripts/ci_install_pyo3_builds.sh
+++ scripts/ci_install_pyo3_builds.sh
@@ -3,7 +3,7 @@ set -eux
 
 install_lib() {
     wget "https://package.cosmian.com/$1/$2/linux.zip"
-    unzip linux.zip
+    unzip -o linux.zip
     pip install --force-reinstall x86_64-unknown-linux-gnu/python-x86_64-unknown-linux-gnu/$1*.whl
     rm linux.zip && rm -rf x86_64-unknown-linux-gnu
 }
