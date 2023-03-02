72868995eb8eac458e9ea5dbbcaffd73a85eb738
Manuthor <manu.coste@gmail.com>
2023-03-02 15:07:07 +0100

    fix: remove FindexCloud

--- src/cloudproof_py/findex/__init__.py
+++ src/cloudproof_py/findex/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from cloudproof_findex import IndexedValue, Label, MasterKey, FindexCloud
+from cloudproof_findex import IndexedValue, Label, MasterKey
 
 from cloudproof_py.findex import Findex, utils
 
-__all__ = ["IndexedValue", "Label", "MasterKey", "Findex", "utils", "FindexCloud"]
+__all__ = ["IndexedValue", "Label", "MasterKey", "Findex", "utils"]
