17012917e00237000f271fb1ae9fd4db8d90bfb7
Manuthor <manu.coste@gmail.com>
2023-03-03 16:07:47 +0100

    fix: download bash scripts

--- scripts/ci_install_pyo3_builds.sh
+++ scripts/ci_install_pyo3_builds.sh
@@ -2,21 +2,13 @@
 set -ux
 
 install_lib() {
-    # wget "https://package.cosmian.com/cloudproof_rust/TODO_$2/linux.zip" &&
-    wget "https://package.cosmian.com/$1/$2/linux.zip" &&
-        unzip -o linux.zip &&
-        pip install --force-reinstall x86_64-unknown-linux-gnu/python-x86_64-unknown-linux-gnu/$1*.whl &&
-        rm linux.zip && rm -rf x86_64-unknown-linux-gnu
+    wget "https://package.cosmian.com/cloudproof_rust/$1/all.zip" &&
+        unzip -o all.zip &&
+        pip install --force-reinstall x86_64-unknown-linux-gnu/python-x86_64-unknown-linux-gnu/*.whl &&
+        rm all.zip && rm -rf x86_64*
 }
 
-install_lib "cover_crypt" "v10.0.0"
-# install_lib "cover_crypt" "v11.0.0"
+install_lib "v1.0.0"
 if [ $? -ne 0 ]; then
-    install_lib "cover_crypt" "last_build"
-fi
-
-install_lib "findex" "v2.0.1"
-# install_lib "findex" "v3.0.0"
-if [ $? -ne 0 ]; then
-    install_lib "findex" "last_build"
+    install_lib "last_build"
 fi
